39a5471702fc1b49748e714f55975088f4044ba7
MinhTam <lait2553@gmail.com>
2023-11-22 22:09:28 +0700

    git initialize

---
+++ src/main/java/vn/edu/iuh/fit/week_06/Week06Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week06Application.class, args);
+    }
+
+}
